b179a03ebc073985578c479b6e34126fb920c050
etudiant <etudiant@MB-R-110-001-176.local>
2023-01-28 19:42:25 +0100

    Ajout d'une action bar pour choisir les filtres de recherche des blagues + modifications de l'activité en question pour prendre en compte l'action bar

--- code/app/src/main/java/fr/iut/jokapp/view/activity/GenerateJokePageActivity.kt
+++ code/app/src/main/java/fr/iut/jokapp/view/activity/GenerateJokePageActivity.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import fr.iut.jokapp.R
 import fr.iut.jokapp.view.callbacks.DisplayJokeCallback
 import fr.iut.jokapp.view.fragments.FragmentDisplayJoke
@@ -25,6 +31,7 @@ class GenerateJokePageActivity : AppCompatActivity(), DisplayJokeCallback {
 
         setContentView(R.layout.layout_generate_joke)
 
+        setSupportActionBar(findViewById(R.id.toolBarGenerateJoke))
         if(savedInstanceState != null) {
             fragmentOptionsJoke = supportFragmentManager.findFragmentById(R.id.fragmentOptions) as FragmentJokeOptions
             fragmentDisplayJoke = supportFragmentManager.findFragmentById(R.id.fragmentDisplayJoke) as FragmentDisplayJoke
@@ -59,6 +66,30 @@ class GenerateJokePageActivity : AppCompatActivity(), DisplayJokeCallback {
             }
         }
         //findViewById<Button>(R.id.buttonJoke).setOnClickListener { apiViewModel.getAnyJoke() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupMenu()
+    }
+
+    private fun setupMenu() {
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_generate_joke, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                /*return when (menuItem.itemId) {
+                    R.id.menu_item_new_dog -> {
+                        addNewDog()
+                        true
+                    }
+                    else -> false
+                }*/
+                return true
+            }
+        }, this, Lifecycle.State.RESUMED)
     }
 
     override fun generateJoke() {
